4e211c904196b72ddff17ba75e38de92af181296
winter <132787889+wintrss@users.noreply.github.com>
2024-09-24 17:30:52 -0700

    prevent video from pausing when going back

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ document.getElementById('enter-screen').addEventListener('click', function() {
     bgVideo.play(); 
     startRGBGlow();
 });
+
 function startRGBGlow() {
     const textElements = [
         ...document.querySelectorAll('.tag'),
@@ -54,11 +55,22 @@ function startRGBGlow() {
         }
     }, 50);
 }
+
 function setVideoVolume(level) {
     const bgVideo = document.getElementById('bg-video');
     bgVideo.volume = level; 
 }
-setVideoVolume(0.1);
+setVideoVolume(0.15);
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('main-content').style.display = 'none'; 
+});
+const bgVideo = document.getElementById('bg-video');
+let lastTime = 0;
+window.addEventListener('pagehide', () => {
+    lastTime = bgVideo.currentTime;
+});
+window.addEventListener('pageshow', () => {
+    bgVideo.currentTime = lastTime;
+    bgVideo.play();
 });
